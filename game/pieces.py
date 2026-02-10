d9df747bbb6fc4ebeac6b54acd665b98ccd81c05
hectoraremil <20242492@itla.edu.do>
2026-02-10 19:04:26 -0400

    se agrega jugadores y piezas

---
+++ game/pieces.py
@@ -0,0 +1,29 @@
+
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Tuple, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from game.player import Player
+
+Position = Tuple[int, int]  
+
+
+@dataclass
+class Piece:
+    owner: "Player"
+    position: Position
+    type: str  
+
+    def move(self, new_position: Position) -> None:
+        self.position = new_position
+
+
+class Master(Piece):
+    def __init__(self, owner: "Player", position: Position):
+        super().__init__(owner=owner, position=position, type="MASTER")
+
+
+class Student(Piece):
+    def __init__(self, owner: "Player", position: Position):
+        super().__init__(owner=owner, position=position, type="STUDENT")
