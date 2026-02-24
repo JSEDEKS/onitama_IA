b2357845f40a06b8c7dc2b9511164b6aac8f387d
SaviHm <135405184+SaviHm@users.noreply.github.com>
2026-02-24 11:13:47 -0400

    Update player.py

--- game/player.py
+++ game/player.py
@@ -3,13 +3,14 @@ from typing import Dict, List, Tuple, Optional
 
 from game.pieces import Master, Student, Piece, Position
 
-
 class Player:
-    def __init__(self, name: str, color: str):
+    def __init__(self, name: str, color: str, is_ai: bool = False, ai_agent=None):
         self.name = name
-        self.color = color  
+        self.color = color
         self.pieces: List[Piece] = []
-        self.cards = []  
+        self.cards = []
+        self.is_ai = is_ai        # True si es jugador IA
+        self.ai_agent = ai_agent  # Instancia de la clase AI correspondiente
 
     def create_pieces(self, positions: Dict[str, object]) -> None:
         """
@@ -19,11 +20,11 @@ class Player:
             "students": [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
         }
         """
-        master_pos = positions["master"]  
-        students_pos = positions["students"]  
+        master_pos = positions["master"]
+        students_pos = positions["students"]
 
-        self.pieces.append(Master(self, master_pos)) 
-        for pos in students_pos:  
+        self.pieces.append(Master(self, master_pos))
+        for pos in students_pos:
             self.pieces.append(Student(self, pos))
 
     def get_master(self) -> Optional[Piece]:
